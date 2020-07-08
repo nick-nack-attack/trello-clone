09301bb4325289706d12acbbc5019abf4a24752f
Nick Farni <nickfarni001@gmail.com>
2020-07-07 18:47:27 -0600

    add use focus on add new item

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,21 +1,28 @@
+// new item form
 import React, { useState } from "react";
+
+// utils
+import { useFocus } from "./utils/useFocus";
+
+// styling
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
 
 interface NewItemFormProps {
     onAdd(text: string): void
 };
 
-export const NewItemForm = (props: NewItemFormProps) => {
-    const [ text, setText ] = useState("");
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+    const [text, setText] = useState("")
+    const inputRef = useFocus()
+  
     return (
-        <NewItemFormContainer>
-            <NewItemInput
-                value={ text }
-                onChange={ e => setText(e.target.value) }
-            />
-            <NewItemButton onClick={() => props.onAdd(text)}>
-                Create
-            </NewItemButton>
-        </NewItemFormContainer>
-    );
-};
+      <NewItemFormContainer>
+        <NewItemInput
+          ref={inputRef}
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+      </NewItemFormContainer>
+    )
+  }
